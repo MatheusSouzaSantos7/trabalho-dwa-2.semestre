2aa58f850af3ce907606f0065190a94991b7e5f5
Matheus Souza <matheus.sax7@gmail.com>
2023-10-02 19:46:10 -0300

    Atualizacao da pagina home, criacao de navigator e criacao de area para criar postaem

--- trabalho-dwa/src/app/app.module.ts
+++ trabalho-dwa/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
+// Importações criadas por Matheus Angelo
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +15,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule // Matheus Souza - Necessário para o formuário de postagem funcionar.
   ],
   providers: [],
   bootstrap: [AppComponent]
--- trabalho-dwa/src/app/home/home.component.ts
+++ trabalho-dwa/src/app/home/home.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  UrlFotoUsuario: string = 'caminho/para/imagem-do-usuario.jpg';
+  NomeUsuario: string = 'Nome do Usuário';
+  NovaPostagem: string = '';
 
+// Ações dos métodos relacionados a criar nova postagem
+  enviarPostagem() {
+    /* Por aqui, deverá enviar o comentário para o back-end e processar a lógica necessária.
+    Você pode usar um serviço para lidar com a chamada HTTP para o back-end.
+    Após o envio bem-sucedido, você pode limpar o campo de comentário, se necessário.
+    Por exemplo, você pode enviar o comentário usando um serviço Angular HttpClient. */
+  }
+
+  limparPostagem() {
+    this.NovaPostagem = ''; // Limpa o campo de comentário
+  }
 }
