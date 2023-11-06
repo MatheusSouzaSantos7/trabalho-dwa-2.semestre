d74cfaca3a2b44471562dc893e7b614c8dfce0e0
Matheus Souza <129551875+MatheusSouzaSantos7@users.noreply.github.com>
2023-11-06 09:37:35 -0300

    Atualizacao para producao. Inclusao arquivo scrpit em dist

---
+++ src/script.js
@@ -0,0 +1,10 @@
+// Exibindo uma mensagem no console
+console.log('O arquivo script.js foi carregado com sucesso!');
+
+// Exemplo de uma função simples
+function helloWorld() {
+  alert('Olá, mundo!');
+}
+
+// Chamando a função helloWorld
+helloWorld();
